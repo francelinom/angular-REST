f7349c9f617f79d2058d68b2c7d4aaf1ef7a3cfc
francelino <fmarcilio1989@gmail.com>
2022-05-18 17:09:54 -0300

    criando o service de usuario e o model

---
+++ src/app/model/user.ts
@@ -0,0 +1,6 @@
+export class User {
+  id?: number;
+  login?: string;
+  nome?: string;
+  cpf?: string;
+}
---
+++ src/app/service/usuario.service.ts
@@ -0,0 +1,15 @@
+import { AppConstants } from './../app-constants';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UsuarioService {
+  constructor(private http: HttpClient) {}
+
+  getList(): Observable<any> {
+    return this.http.get<any>(AppConstants.baseUrl);
+  }
+}
