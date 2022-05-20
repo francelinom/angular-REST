534999ae255b410c33072605bc2a56a793c32a0d
francelino <fmarcilio1989@gmail.com>
2022-05-20 17:09:09 -0300

    tratamento de erros vindo do back

--- src/app/service/header-interceptor.service.ts
+++ src/app/service/header-interceptor.service.ts
@@ -1,12 +1,15 @@
 import {
+  HttpErrorResponse,
   HttpEvent,
   HttpHandler,
   HttpInterceptor,
   HttpRequest,
+  HttpResponse,
   HTTP_INTERCEPTORS,
 } from '@angular/common/http';
 import { Injectable, NgModule } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
@@ -24,10 +27,33 @@ export class HeaderInterceptorService implements HttpInterceptor {
         headers: req.headers.set('Authorization', token),
       });
 
-      return next.handle(tokenRequest);
+      return next.handle(tokenRequest).pipe(
+        tap((event: HttpEvent<any>) => {
+          if (
+            event instanceof HttpResponse &&
+            (event.status === 200 || event.status === 2001)
+          ) {
+            console.log('Operação com sucesso!');
+          }
+        }),
+        catchError(this.processaError)
+      );
     } else {
-      return next.handle(req);
+      return next.handle(req).pipe(catchError(this.processaError));
     }
+  }
+
+  processaError(error: HttpErrorResponse) {
+    let errorMessage = 'Erro desconhecido';
+    if (error.error instanceof ErrorEvent) {
+      console.log(error.error);
+      errorMessage = 'Error: ' + error.error.error;
+    } else {
+      errorMessage =
+        'Código ' + error.error.code + '\nMensagem: ' + error.error.error;
+    }
+    window.alert(errorMessage);
+    return throwError(errorMessage);
   }
 }
 
